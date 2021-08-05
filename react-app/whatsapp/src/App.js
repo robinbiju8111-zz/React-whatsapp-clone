abb8311d9eff6f79e4037338de219fa6624bd70a
Robin Biju <robinbiju8111@gmaili.com>
2021-08-05 19:34:34 +0530

    initial commit

---
+++ react-app/whatsapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-body">
+        {/* sidebar*/}
+        {/* chat screen*/}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app/whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9sO-k_vq5tqFQqsJcv7p00l8nx3AZkuo",
+    authDomain: "whatsapp-clone-ba953.firebaseapp.com",
+    projectId: "whatsapp-clone-ba953",
+    storageBucket: "whatsapp-clone-ba953.appspot.com",
+    messagingSenderId: "993453969587",
+    appId: "1:993453969587:web:e02b97492b7b3e901360c6",
+    measurementId: "G-SWXB1JJNE9"
+  };
